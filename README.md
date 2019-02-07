0ab331c6931db1b26e53533465abe9ddf1ec99ca
Nikola Derežić <nikola.derezic@gmail.com>
2019-02-07 15:04:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Update Chunk Links In Files Webpack Plugin
+This plugin replaces chunk file names (links) in given list of files. 
+
+It's meant to solve the problem of updating file names inside HTML / ASPX / PHP / etc. files when chunk names contain a hash, which changes every time a new version of the file is created by WebPack.
+
+In case you are not using Webpack to build your HTML / ASPX / PHP files, this plugin will solve that problem!
+
+## Installation
+```shell
+$ npm install update-chunk-links-in-files-webpack-plugin --save-dev
+```
+
+## Example
+
+### Webpack.config.js
+
+First let's have a look at WebPack config file (unimportant bits are omitted for brevity).
+
+Notice that the file names in the `output` section contain `[contenthash]`, which changes each time the content of the file changes.
+
+```javascript
+const path = require('path');
+const UpdateChunkLinksInFilesWebpackPlugin = require('update-chunk-links-in-files-webpack-plugin');
+
+module.exports = {
+
+    entry: {
+        "app": path.resolve("src/index.js")
+    },
+    output: {
+        // chunk names contain `contenthash`
+        filename: "[name].[contenthash].bundle.js",
+        chunkFilename: "[name].[contenthash].bundle.js"
+    },
+
+    /* ... your usual WebPack stuff goes here ... */
+
+    plugins: [
+        new UpdateChunkLinksInFilesWebpackPlugin({
+            // files containing links to chunks, which need to be updated
+            replaceInFiles: [
+                path.resolve(__dirname, '_site/master-pages/default.aspx')
+            ],
+            // regexp patterns to be used to find chunk names in the file
+            assetsPatterns: [
+                /vendor\.[a-z0-9]+\.bundle\.js/g,
+                /app\.[a-z0-9]+\.bundle\.js/g
+            ]
+        })
+    ]
+};
+```
+
+### HTML
+The following snippet shows the `default.aspx` file, which is indicated in the WebPack config
+
+Each time any of the bundle files changes the plugin will update the *ASPX* file accordingly.
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <p>Hello World</p>
+    <script src="/script/vendor.0f8f98e02847fe61032d.bundle.js"></script>
+    <script src="/script/app.f0c4c1d45df3e888a309.bundle.js" async></script>
+</body>
+</html>
+```
+
+# License
+MIT
---
+++ index.js
@@ -0,0 +1,82 @@
+function UpdateChunkLinksInFilesWebpackPlugin(options) {
+    this.options = options;
+}
+
+UpdateChunkLinksInFilesWebpackPlugin.prototype.apply = function(compiler) {
+
+    compiler.hooks.emit.tapAsync("UpdateChunkLinksInFilesWebpackPlugin", (compilation, callback) => {
+
+        const {replaceInFiles, chunkNamePatterns, enabled, disabled} = this.options;
+
+        if(enabled===false || disabled===true) {
+            callback(); // passing the control back to WebPack
+            return; // do nothing
+        }
+
+        // Find chunks which match the pattern
+        // IF the `chunkFilename` is matched with any pattern
+        // the it will be mapped new object `{pattern, chunkFilename}` (created by `reduce`)
+        // ELSE IF no match is found it will be mapped to `false` (returned by `reduce`)
+        let matchingChunks = Object.keys(compilation.assets)
+            .map(chunkFilename =>
+                // `reduce` tries to match the given chunk with one of the patterns
+                chunkNamePatterns.reduce((prevResult, pattern) => !prevResult && pattern.test(chunkFilename) ? {chunkFilename, pattern} : prevResult, false))
+            // keep only `chunkFilename`s which were matched by a pattern
+            .filter(chunkFilename => !!chunkFilename)
+
+        if(matchingChunks.length > 0) {
+
+            // tracking how many files have been processed
+            let filesLeftToRead = replaceInFiles.length,
+                filesLeftToWrite = filesLeftToRead;
+
+            replaceInFiles.forEach(oneFile => {
+
+                fs.readFile(oneFile, 'utf8', function (err, data) {
+                    filesLeftToRead--;
+
+                    // check if the read was successful
+                    if (err) {
+
+                        filesLeftToWrite--; // the current file will never be written to
+
+                        console.log(`UpdateChunkLinksInFilesWebpackPlugin: error reading from file ${oneFile} not found`, err);
+
+                        if(filesLeftToRead===0 && filesLeftToWrite===0) {
+                            // IF all the files have been processed
+                            // > passing the control back to WebPack
+                            callback();
+                        }
+
+                        return; // stop processing the current file
+                    }
+
+                    // do a search-replace for every matching chunk
+                    matchingChunks.forEach(({pattern, chunkFilename}) => {
+                        data = data.replace(pattern, chunkFilename);
+                    });
+
+                    fs.writeFile(oneFile, data, 'utf8', err => {
+
+                        --filesLeftToWrite;
+
+                        if (err) {
+                            console.log(`UpdateChunkLinksInFilesWebpackPlugin: error writing to file ${oneFile}`, err);
+                        }
+
+                        if(filesLeftToRead===0 && filesLeftToWrite===0) {
+                            callback(); // passing the control back to WebPack
+                        }
+                    });
+                });
+            });
+        } else {
+            console.log(`UpdateChunkLinksInFilesWebpackPlugin: no matching chunks found`);
+            // ELSE - no chunk which matched the given rules
+            // > passing the control back to WebPack
+            callback();
+        }
+    });
+};
+    
+module.exports = UpdateChunkLinksInFilesWebpackPlugin;
